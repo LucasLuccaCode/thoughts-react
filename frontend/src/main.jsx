ef50472815131c3320b27606a172525db0b2e4a4
Lucca Santos <lucasluccad.r@outlook.com>
2023-01-19 10:28:08 -0300

    refactor: route structure moved to a separate file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,68 +1,12 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import { AuthProvider } from './contexts/auth'
 import { MessageProvider } from './contexts/message'
 
-// Pages
-import Root from './pages/Root'
-import NotFound from './pages/NotFound'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import DashboardCreate from './pages/Dashboard/CreateThought'
-import DashboardEditThought from './pages/Dashboard/EditThought'
-
-import { PrivateRoute } from "./routes/privateRoutes"
-import { AuthProvider } from './contexts/auth'
-
-import Home, {
-  loader as homeLoader
-} from './pages/Home'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-        loader: homeLoader
-      },
-      {
-        path: "/dashboard",
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: "/dashboard",
-            element: <Dashboard />,
-            children: [
-              {
-                path: "add",
-                element: <DashboardCreate />
-              },
-              {
-                path: ":thoughtId/edit",
-                element: <DashboardEditThought />
-              }
-            ]
-          }
-        ]
-      },
-      {
-        path: "/login",
-        element: <Login />
-      },
-      {
-        path: "/register",
-        element: <Register />
-      }
-    ]
-  }
-])
+import router from './routes/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,58 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+// Pages
+import Root from '../pages/Root'
+import NotFound from '../pages/NotFound'
+import Register from '../pages/Register'
+import Login from '../pages/Login'
+import Dashboard from '../pages/Dashboard'
+import DashboardCreate from '../pages/Dashboard/CreateThought'
+import DashboardEditThought from '../pages/Dashboard/EditThought'
+import Home, { loader as homeLoader } from '../pages/Home'
+
+import { PrivateRoute } from "./privateRoutes"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: homeLoader
+      },
+      {
+        path: "/dashboard",
+        element: <PrivateRoute />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+            children: [
+              {
+                path: "add",
+                element: <DashboardCreate />
+              },
+              {
+                path: ":thoughtId/edit",
+                element: <DashboardEditThought />
+              }
+            ]
+          }
+        ]
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/register",
+        element: <Register />
+      }
+    ]
+  }
+])
+
+export default router
